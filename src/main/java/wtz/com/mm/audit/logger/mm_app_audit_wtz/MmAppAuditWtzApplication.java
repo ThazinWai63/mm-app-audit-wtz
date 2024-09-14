ba9e7d40728047472d8387dd61096f95d705feee
ThazinWai63 <e1332403@u.nus.edu>
2024-09-14 11:13:44 +0800

    Uploading the scratch project

---
+++ src/main/java/wtz/com/mm/audit/logger/mm_app_audit_wtz/MmAppAuditWtzApplication.java
@@ -0,0 +1,13 @@
+package wtz.com.mm.audit.logger.mm_app_audit_wtz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmAppAuditWtzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmAppAuditWtzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wtz/com/mm/audit/logger/mm_app_audit_wtz/MmAppAuditWtzApplicationTests.java
@@ -0,0 +1,13 @@
+package wtz.com.mm.audit.logger.mm_app_audit_wtz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmAppAuditWtzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
